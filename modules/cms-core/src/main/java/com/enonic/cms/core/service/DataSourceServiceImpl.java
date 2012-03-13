4f49b4cf51f240783696fc5259f00574a36a016e
Runar Myklebust <rmy@enonic.com>
2012-03-13 12:52:49 +0100

    Stupid-award 2012 to rmy

--- modules/cms-core/src/main/java/com/enonic/cms/core/service/DataSourceServiceImpl.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/service/DataSourceServiceImpl.java
@@ -1638,14 +1638,6 @@ public final class DataSourceServiceImpl
             xmlCreator.setIncludeVersionsInfoForPortal( false );
             xmlCreator.setIncludeAssignment( true );
 
-            try
-            {
-                Thread.sleep( 2000 );
-            }
-            catch ( InterruptedException e )
-            {
-            }
-
             ContentResultSet contents = contentService.queryContent( spec );
             if ( previewContext.isPreviewingContent() )
             {

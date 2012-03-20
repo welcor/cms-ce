237a4f3edf49b8f73976e7d78998453d3aa3158d
Runar Myklebust <rmy@enonic.com>
2012-03-20 10:04:20 +0100

    B-02731: Integrate Elasticsearch: Apply binary data to index

--- modules/cms-core/src/test/java/com/enonic/cms/core/search/query/QueryTranslatorTest_attachments.java
+++ modules/cms-core/src/test/java/com/enonic/cms/core/search/query/QueryTranslatorTest_attachments.java
@@ -13,9 +13,49 @@ public class QueryTranslatorTest_attachments
     public void testAttachmentQuery()
         throws Exception
     {
-        String expected_search_result = "";
+        String expected_search_result = "{\n" +
+            "  \"from\" : 0,\n" +
+            "  \"size\" : 200,\n" +
+            "  \"query\" : {\n" +
+            "    \"has_child\" : {\n" +
+            "      \"query\" : {\n" +
+            "        \"term\" : {\n" +
+            "          \"_all\" : \"test\"\n" +
+            "        }\n" +
+            "      },\n" +
+            "      \"type\" : \"binaries\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
 
         ContentIndexQuery query = createContentQuery( "attachment/* = 'test'" );
+
+        SearchSourceBuilder builder = getQueryTranslator().build( query );
+
+        compareStringsIgnoreFormatting( expected_search_result, builder.toString() );
+
+    }
+
+    @Test
+    public void testAliasedAttachementField()
+        throws Exception
+    {
+        String expected_search_result = "{\n" +
+            "  \"from\" : 0,\n" +
+            "  \"size\" : 200,\n" +
+            "  \"query\" : {\n" +
+            "    \"has_child\" : {\n" +
+            "      \"query\" : {\n" +
+            "        \"term\" : {\n" +
+            "          \"attachment\" : \"test\"\n" +
+            "        }\n" +
+            "      },\n" +
+            "      \"type\" : \"binaries\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+        ContentIndexQuery query = createContentQuery( "fulltext = 'test'" );
 
         SearchSourceBuilder builder = getQueryTranslator().build( query );
 

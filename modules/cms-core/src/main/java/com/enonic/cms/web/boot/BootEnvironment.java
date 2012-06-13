d5513efeca742d34d3075544aab98e9b54d1b00a
Sten Roger Sandvik <srs@enonic.com>
2012-06-13 13:31:41 +0200

    Added banner with OS and Java info

--- modules/cms-core/src/main/java/com/enonic/cms/web/boot/BootEnvironment.java
+++ modules/cms-core/src/main/java/com/enonic/cms/web/boot/BootEnvironment.java
@@ -1,14 +1,25 @@
 package com.enonic.cms.web.boot;
 
-import com.enonic.cms.core.home.HomeDir;
+import org.apache.commons.lang.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.enonic.cms.core.home.HomeResolver;
+import com.enonic.cms.core.product.ProductVersion;
 
 final class BootEnvironment
 {
-    private HomeDir homeDir;
+    private final static Logger LOG = LoggerFactory.getLogger( BootEnvironment.class );
+
+    private final static String BANNER = "\n" +
+        " _______ _______ _______ _______ _______ ______   ______ _______ _______ \n" +
+        "|    ___|    |  |       |    |  |_     _|      | |      |   |   |     __|\n" +
+        "|    ___|       |   -   |       |_|   |_|   ---| |   ---|       |__     |\n" +
+        "|_______|__|____|_______|__|____|_______|______| |______|__|_|__|_______|\n\n";
 
     public void initialize()
     {
+        logBanner();
         resolveHomeDir();
     }
 
@@ -20,6 +31,32 @@ final class BootEnvironment
     private void resolveHomeDir()
     {
         final HomeResolver resolver = new HomeResolver();
-        this.homeDir = resolver.resolve();
+        resolver.resolve();
+    }
+
+    private void logBanner()
+    {
+        final StringBuilder str = new StringBuilder( BANNER );
+        str.append( "  # " ).append( ProductVersion.getFullTitleAndVersion() ).append( "\n" );
+        str.append( "  # " ).append( getFormattedJvmInfo() ).append( "\n" );
+        str.append( "  # " ).append( getFormattedOsInfo() ).append( "\n" );
+
+        LOG.info( str.toString() );
+    }
+
+    private String getFormattedJvmInfo()
+    {
+        final StringBuilder str = new StringBuilder();
+        str.append( SystemUtils.JAVA_RUNTIME_NAME ).append( " " ).append( SystemUtils.JAVA_RUNTIME_VERSION ).append( " (" ).append(
+            SystemUtils.JAVA_VENDOR ).append( ")" );
+        return str.toString();
+    }
+
+    private String getFormattedOsInfo()
+    {
+        final StringBuilder str = new StringBuilder();
+        str.append( SystemUtils.OS_NAME ).append( " " ).append( SystemUtils.OS_NAME ).append( " (" ).append(
+            SystemUtils.OS_ARCH ).append( ")" );
+        return str.toString();
     }
 }

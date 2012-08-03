e73667df588012695ffa922cd0baca9735d585b0
Sten Roger Sandvik <srs@enonic.com>
2012-08-03 10:57:25 +0200

    Removed the use of MemoryBlobStore. Integration tests will now use a "real" homeDir so no need to do it differently in integration tests anymore.

--- modules/cms-core/src/main/java/com/enonic/cms/framework/blob/BlobStoreBeans.java
+++ modules/cms-core/src/main/java/com/enonic/cms/framework/blob/BlobStoreBeans.java
@@ -5,18 +5,16 @@ import java.io.File;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.enonic.cms.framework.blob.file.FileBlobStore;
 
 @Configuration
-@Profile("default")
 public class BlobStoreBeans
 {
     @Bean
-    public BlobStore blobStore( @Value("${cms.blobstore.dir}") File directory )
+    public BlobStore blobStore( final @Value("${cms.blobstore.dir}") File directory )
     {
-        FileBlobStore fileBlobStore = new FileBlobStore();
+        final FileBlobStore fileBlobStore = new FileBlobStore();
         fileBlobStore.setDirectory( directory );
         return fileBlobStore;
     }

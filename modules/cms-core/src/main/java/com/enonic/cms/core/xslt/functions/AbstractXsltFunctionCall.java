4beb346d7e6dce2cf8f4f4363ba1a473fa45c694
Sten Roger Sandvik <srs@enonic.com>
2012-08-20 09:14:38 +0200

    Removed some commented lines.

--- modules/cms-core/src/main/java/com/enonic/cms/core/xslt/functions/AbstractXsltFunctionCall.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/xslt/functions/AbstractXsltFunctionCall.java
@@ -142,48 +142,4 @@ public abstract class AbstractXsltFunctionCall
 
         return list.toArray( new String[list.size()] );
     }
-
-    /*
-    protected final SequenceIterator<? extends Item> createValue( final String value )
-    {
-        return StringValue.makeStringValue( value ).iterate();
-    }
-
-    protected final SequenceIterator<? extends Item> createValue( final boolean value )
-    {
-        return BooleanValue.get( value ).iterate();
-    }
-
-    protected final String toSingleString( final SequenceIterator<? extends Item> sequence )
-        throws XPathException
-    {
-        final Item item = sequence.next();
-        if ( item != null )
-        {
-            return item.getStringValue();
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    protected final String[] toStringArray( final SequenceIterator<? extends Item> sequence )
-        throws XPathException
-    {
-        final ArrayList<String> list = new ArrayList<String>();
-
-        while ( true )
-        {
-            final String value = toSingleString( sequence );
-            if ( value == null )
-            {
-                break;
-            }
-
-            list.add( value );
-        }
-
-        return list.toArray( new String[list.size()] );
-    }*/
 }

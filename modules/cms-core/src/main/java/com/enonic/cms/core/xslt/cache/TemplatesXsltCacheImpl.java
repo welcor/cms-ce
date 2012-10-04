27644cccf9d68d698a52febe16393897573aa2bc
Sten Roger Sandvik <srs@enonic.com>
2012-10-04 09:33:53 +0200

    Reverted back to old fashion javax templates for xslt.

--- modules/cms-core/src/main/java/com/enonic/cms/core/xslt/cache/TemplatesXsltCacheImpl.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/xslt/cache/TemplatesXsltCacheImpl.java
@@ -1,9 +1,9 @@
 package com.enonic.cms.core.xslt.cache;
 
+import javax.xml.transform.Templates;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import net.sf.saxon.s9api.XsltExecutable;
 
 import com.enonic.cms.framework.cache.CacheFacade;
 import com.enonic.cms.framework.cache.CacheManager;
@@ -17,13 +17,13 @@ public final class TemplatesXsltCacheImpl
     private CacheFacade cacheFacade;
 
     @Override
-    public XsltExecutable get( final XsltResource xsl )
+    public Templates get( final XsltResource xsl )
     {
-        return (XsltExecutable) this.cacheFacade.get( null, xsl.getName() );
+        return (Templates) this.cacheFacade.get( null, xsl.getName() );
     }
 
     @Override
-    public void put( final XsltResource xsl, final XsltExecutable templates )
+    public void put( final XsltResource xsl, final Templates templates )
     {
         this.cacheFacade.put( null, xsl.getName(), templates );
     }

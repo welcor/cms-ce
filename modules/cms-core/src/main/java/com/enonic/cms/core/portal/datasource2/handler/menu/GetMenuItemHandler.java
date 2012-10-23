f5d7c3c5e3e85542137994d941c9cd7a2ffbb842
Alex Rodriguez <aro@enonic.com>
2012-10-23 15:11:08 +0200

    CMS-429 Implement getMenuItem datasource handler

--- modules/cms-core/src/main/java/com/enonic/cms/core/portal/datasource2/handler/menu/GetMenuItemHandler.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/portal/datasource2/handler/menu/GetMenuItemHandler.java
@@ -1,13 +1,18 @@
 package com.enonic.cms.core.portal.datasource2.handler.menu;
 
 import org.jdom.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.enonic.cms.core.portal.datasource2.handler.DataSourceHandler;
 import com.enonic.cms.core.portal.datasource2.handler.DataSourceRequest;
+import com.enonic.cms.core.service.DataSourceService;
 
 public final class GetMenuItemHandler
     extends DataSourceHandler
 {
+
+    private DataSourceService dataSourceService;
+
     public GetMenuItemHandler()
     {
         super( "getMenuItem" );
@@ -18,10 +23,15 @@ public final class GetMenuItemHandler
         throws Exception
     {
         final int menuItemKey = req.param( "menuItemKey" ).required().asInteger();
-        final boolean tagItem = req.param( "withParents" ).asBoolean( false );
+        final boolean withParents = req.param( "withParents" ).asBoolean( false );
         final boolean details = req.param( "details" ).asBoolean( false );
 
-        // TODO: Implement based on DataSourceServiceImpl.getMenuItem(..)
-        return null;
+        return this.dataSourceService.getMenuItem( req, menuItemKey, withParents, details).getAsJDOMDocument();
+    }
+
+    @Autowired
+    public void setDataSourceService( final DataSourceService dataSourceService )
+    {
+        this.dataSourceService = dataSourceService;
     }
 }

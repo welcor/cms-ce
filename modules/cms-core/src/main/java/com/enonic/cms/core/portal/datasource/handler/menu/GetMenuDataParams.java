ae0fae72ccfdb44a6f5d1cd9a5baa912f2ec6761
Henady Zakalusky <hza@enonic.com>
2012-11-21 15:39:24 +0300

    CMS-614 : Make siteKey optional in getMenuData datasource

--- modules/cms-core/src/main/java/com/enonic/cms/core/portal/datasource/handler/menu/GetMenuDataParams.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/portal/datasource/handler/menu/GetMenuDataParams.java
@@ -1,9 +1,6 @@
 package com.enonic.cms.core.portal.datasource.handler.menu;
 
-import javax.annotation.Nonnull;
-
 public final class GetMenuDataParams
 {
-    @Nonnull
     public Integer siteKey;
 }

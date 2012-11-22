8e2956d2dd710cfe9453faf8fa60c6f8f97b680d
Runar Myklebust <rmy@enonic.com>
2012-11-22 10:52:55 +0100

    CMS-618 Implement histogram facet
    Added test for es-builder

---
+++ modules/cms-core/src/test/java/com/enonic/cms/core/search/facet/builder/ElasticsearchHistogramFacetBuilderTest.java
@@ -0,0 +1,61 @@
+package com.enonic.cms.core.search.facet.builder;
+
+import org.elasticsearch.search.facet.histogram.HistogramFacetBuilder;
+import org.junit.Test;
+
+import com.enonic.cms.core.search.facet.AbstractElasticsearchFacetTestBase;
+import com.enonic.cms.core.search.facet.model.HistogramFacetModel;
+
+import static org.junit.Assert.*;
+
+public class ElasticsearchHistogramFacetBuilderTest
+    extends AbstractElasticsearchFacetTestBase
+{
+
+    @Test
+    public void field()
+        throws Exception
+    {
+        ElasticsearchHistogramFacetBuilder facetBuilder = new ElasticsearchHistogramFacetBuilder();
+
+        String expectedJson = "{\"myHistogramFacet\":{\"histogram\":{\"field\":\"myfield.number\",\"interval\":100}}}";
+
+        HistogramFacetModel model = new HistogramFacetModel();
+        model.setName( "myHistogramFacet" );
+        model.setIndex( "myField" );
+        model.setCount( 10 );
+
+        model.setInterval( 100L );
+
+        final HistogramFacetBuilder histogramFacetBuilder = facetBuilder.build( model );
+
+        final String json = getJson( histogramFacetBuilder );
+
+        assertEquals( expectedJson, json );
+    }
+
+    @Test
+    public void key_valuefield()
+        throws Exception
+    {
+        ElasticsearchHistogramFacetBuilder facetBuilder = new ElasticsearchHistogramFacetBuilder();
+
+        String expectedJson =
+            "{\"myHistogramFacet\":{\"histogram\":{\"key_field\":\"data_mykeyfield.number\",\"value_field\":\"data_myvaluefield.number\",\"interval\":100}}}";
+
+        HistogramFacetModel model = new HistogramFacetModel();
+        model.setName( "myHistogramFacet" );
+        model.setCount( 10 );
+        model.setInterval( 100L );
+        model.setKeyField( "data/myKeyField" );
+        model.setValueField( "data/myValueField" );
+
+        final HistogramFacetBuilder histogramFacetBuilder = facetBuilder.build( model );
+
+        final String json = getJson( histogramFacetBuilder );
+
+        assertEquals( expectedJson, json );
+    }
+
+
+}

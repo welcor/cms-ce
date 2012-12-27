4ee6e2771b06ba51f8824c41c6d9df3926625fd6
Sten Roger Sandvik <srs@enonic.com>
2012-12-27 14:05:39 +0100

    Fixed so that connectionFactory can be used without hibernate.

--- modules/cms-core/src/main/java/com/enonic/cms/store/support/ConnectionFactory.java
+++ modules/cms-core/src/main/java/com/enonic/cms/store/support/ConnectionFactory.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import com.enonic.cms.framework.jdbc.ConnectionDecorator;
 
@@ -42,7 +43,13 @@ public final class ConnectionFactory
     public Connection getConnection( boolean decorated )
         throws SQLException
     {
-        final Connection conn = this.sessionFactory.getCurrentSession().connection();
+        if ( this.sessionFactory != null )
+        {
+            final Connection conn = this.sessionFactory.getCurrentSession().connection();
+            return decorated ? this.decorator.decorate( conn ) : conn;
+        }
+
+        final Connection conn = DataSourceUtils.getConnection( this.dataSource );
         return decorated ? this.decorator.decorate( conn ) : conn;
     }
 

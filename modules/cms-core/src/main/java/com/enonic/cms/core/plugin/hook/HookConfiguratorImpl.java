a04b470ef606e9e36970be6c9713ce5ea0d4d723
Sten Roger Sandvik <srs@enonic.com>
2013-04-19 23:28:43 +0200

    CMS-1452 Class.getPackage() returns null in Plugin environment.

--- modules/cms-core/src/main/java/com/enonic/cms/core/plugin/hook/HookConfiguratorImpl.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/plugin/hook/HookConfiguratorImpl.java
@@ -1,5 +1,6 @@
 package com.enonic.cms.core.plugin.hook;
 
+import org.eclipse.core.runtime.internal.adaptor.EclipseClassLoadingHook;
 import org.eclipse.osgi.baseadaptor.HookConfigurator;
 import org.eclipse.osgi.baseadaptor.HookRegistry;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ public final class HookConfiguratorImpl
     {
         registry.addAdaptorHook( new LoggingHook() );
         registry.addBundleFileWrapperFactoryHook( new TransformerHook() );
+        registry.addClassLoadingHook( new EclipseClassLoadingHook() );
         registry.addClassLoaderDelegateHook( new ParentClassLoaderHook() );
     }
 }

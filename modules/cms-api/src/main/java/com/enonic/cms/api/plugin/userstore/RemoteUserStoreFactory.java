509088815e744e126461819b6f1f12f88efc3e29
Sten Roger Sandvik <srs@enonic.com>
2013-08-05 15:12:10 +0200

    CMS-1926 Make RemoteUserStore pluggable. Added RemoteUserStoreFactory to API.

---
+++ modules/cms-api/src/main/java/com/enonic/cms/api/plugin/userstore/RemoteUserStoreFactory.java
@@ -0,0 +1,55 @@
+package com.enonic.cms.api.plugin.userstore;
+
+import java.util.Properties;
+
+import com.enonic.cms.api.plugin.ext.Extension;
+
+public abstract class RemoteUserStoreFactory
+    implements Extension, Comparable<RemoteUserStoreFactory>
+{
+    private final String type;
+
+    private final String[] aliases;
+
+    public RemoteUserStoreFactory( final String type, final String... aliases )
+    {
+        this.type = type;
+        this.aliases = aliases;
+    }
+
+    public final String getType()
+    {
+        return this.type;
+    }
+
+    public final String[] getAliases()
+    {
+        return this.aliases;
+    }
+
+    public final boolean isOfType( final String type )
+    {
+        if ( this.type.equalsIgnoreCase( type ) )
+        {
+            return true;
+        }
+
+        for ( final String alias : this.aliases )
+        {
+            if ( alias.equalsIgnoreCase( type ) )
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public abstract RemoteUserStore create( Properties props );
+
+    @Override
+    public final int compareTo( final RemoteUserStoreFactory o )
+    {
+        return this.type.compareTo( o.type );
+    }
+}

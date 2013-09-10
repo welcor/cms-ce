46f1f8aa5806266e67996cfc415f9a56cf86d916
Runar Myklebust <rmy@enonic.com>
2013-09-10 14:40:56 +0200

    CMS-2022 Add index info to status page
    Ok, did it more properly then

--- modules/cms-core/src/main/java/com/enonic/cms/web/status/builders/IndexStatusInfoBuilder.java
+++ modules/cms-core/src/main/java/com/enonic/cms/web/status/builders/IndexStatusInfoBuilder.java
@@ -24,38 +24,57 @@ public class IndexStatusInfoBuilder
     @Override
     protected void build( final ObjectNode json )
     {
+        buildClusterHealtStatus( json );
+        buildIndexStatus( json );
+    }
 
-        final ClusterHealthResponse clusterHealthResponse = elasticSearchIndexService.getClusterHealth( "cms", false );
-
-        if ( clusterHealthResponse == null )
-        {
-            json.put( "error", "not able to get cluster health response" );
-            return;
-        }
-
-        json.put( "status", clusterHealthResponse.getStatus().toString() );
-        json.put( "activeShards", clusterHealthResponse.getActiveShards() );
-        json.put( "activePrimaryShards", clusterHealthResponse.getActivePrimaryShards() );
-        json.put( "activeReplicas", clusterHealthResponse.getActiveShards() - clusterHealthResponse.getActivePrimaryShards() );
-        json.put( "unassignedShards", clusterHealthResponse.getUnassignedShards() );
-        json.put( "relocatingShards", clusterHealthResponse.getRelocatingShards() );
-        json.put( "initializingShards", clusterHealthResponse.getInitializingShards() );
-
+    private void buildIndexStatus( final ObjectNode json )
+    {
         final IndexStatus indexStatus = elasticSearchIndexService.getIndexStatus( "cms" );
 
         if ( indexStatus == null )
         {
-            json.put( "error", "not able to fetch indexStatus" );
-            return;
+            json.put( "documents", "error : not able to fetch indexStatus" );
+            json.put( "primaryShardsStoreSize", "error : not able to fetch indexStatus" );
+            json.put( "totalStoreSize", "error : not able to fetch indexStatus" );
         }
+        else
+        {
+            json.put( "documents", indexStatus.getDocs() == null
+                ? "error: not able to get number of documents"
+                : indexStatus.getDocs().getNumDocs() + "" );
+            json.put( "primaryShardsStoreSize", indexStatus.getPrimaryStoreSize() == null
+                ? "error: not able to get primaryStoreSize"
+                : indexStatus.getPrimaryStoreSize().toString() );
+            json.put( "totalStoreSize",
+                      indexStatus.getStoreSize() == null ? "error: not able to get storeSize" : indexStatus.getStoreSize().toString() );
+        }
+    }
 
-        json.put( "documents",
-                  indexStatus.getDocs() == null ? "error: not able to get number of documents" : indexStatus.getDocs().getNumDocs() + "" );
-        json.put( "primaryShardsStoreSize", indexStatus.getPrimaryStoreSize() == null
-            ? "error: not able to get primaryStoreSize"
-            : indexStatus.getPrimaryStoreSize().toString() );
-        json.put( "totalStoreSize",
-                  indexStatus.getStoreSize() == null ? "error: not able to get storeSize" : indexStatus.getStoreSize().toString() );
+    private void buildClusterHealtStatus( final ObjectNode json )
+    {
+        final ClusterHealthResponse clusterHealthResponse = elasticSearchIndexService.getClusterHealth( "cms", false );
+
+        if ( clusterHealthResponse == null )
+        {
+            json.put( "status", "error : not able to fetch clusterHealthResponse" );
+            json.put( "activeShards", "error : not able to fetch clusterHealthResponse" );
+            json.put( "activePrimaryShards", "error : not able to fetch clusterHealthResponse" );
+            json.put( "activeReplicas", "error : not able to fetch clusterHealthResponse" );
+            json.put( "unassignedShards", "error : not able to fetch clusterHealthResponse" );
+            json.put( "relocatingShards", "error : not able to fetch clusterHealthResponse" );
+            json.put( "initializingShards", "error : not able to fetch clusterHealthResponse" );
+        }
+        else
+        {
+            json.put( "status", clusterHealthResponse.getStatus().toString() );
+            json.put( "activeShards", clusterHealthResponse.getActiveShards() );
+            json.put( "activePrimaryShards", clusterHealthResponse.getActivePrimaryShards() );
+            json.put( "activeReplicas", clusterHealthResponse.getActiveShards() - clusterHealthResponse.getActivePrimaryShards() );
+            json.put( "unassignedShards", clusterHealthResponse.getUnassignedShards() );
+            json.put( "relocatingShards", clusterHealthResponse.getRelocatingShards() );
+            json.put( "initializingShards", clusterHealthResponse.getInitializingShards() );
+        }
     }
 
 }

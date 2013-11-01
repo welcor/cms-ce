838e3c8d8457ab4fa7e66d24536d7913d01eb349
Runar Myklebust <rmy@enonic.com>
2013-11-01 12:44:37 +0100

    CMS-2360 Slow queries or OutOfMemory when content published to a lot of sections
    Added check for number of sections published to before doing earger-fetching joining the 4 tables to prevent too many rows in result

--- modules/cms-core/src/main/java/com/enonic/cms/store/dao/FindContentByKeysQuerier.java
+++ modules/cms-core/src/main/java/com/enonic/cms/store/dao/FindContentByKeysQuerier.java
@@ -11,15 +11,21 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 import com.enonic.cms.framework.hibernate.support.InClauseBuilder;
 import com.enonic.cms.framework.hibernate.support.SelectBuilder;
 
 import com.enonic.cms.core.content.ContentEntity;
 import com.enonic.cms.core.content.ContentKey;
+import com.enonic.cms.core.structure.menuitem.section.SectionContentEntity;
 
 class FindContentByKeysQuerier
 {
+
+    public static final int EAGER_FETCH_NUMBER_OF_SECTIONS_THRESHOLD = 10;
+
     private Session hibernateSession;
 
     private ContentEagerFetches contentEagerFetches;
@@ -39,26 +45,31 @@ class FindContentByKeysQuerier
         final SelectBuilder hqlQuery = new SelectBuilder( 0 );
         hqlQuery.addSelect( "c" );
         hqlQuery.addFromTable( ContentEntity.class.getName(), "c", SelectBuilder.NO_JOIN, null );
-        if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.ACCESS ) )
+
+        if ( eagerFetchingIsSafe( contentKeys ) )
         {
-            hqlQuery.addFromTable( "c.contentAccessRights", null, SelectBuilder.LEFT_JOIN_FETCH, null );
+            if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.ACCESS ) )
+            {
+                hqlQuery.addFromTable( "c.contentAccessRights", null, SelectBuilder.LEFT_JOIN_FETCH, null );
+            }
+            if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.MAIN_VERSION ) )
+            {
+                hqlQuery.addFromTable( "c.mainVersion", null, SelectBuilder.LEFT_JOIN_FETCH, null );
+            }
+            if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.SECTION_CONTENT ) )
+            {
+                hqlQuery.addFromTable( "c.sectionContents", null, SelectBuilder.LEFT_JOIN_FETCH, null );
+            }
+            if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.DIRECT_MENUITEM_PLACEMENT ) )
+            {
+                hqlQuery.addFromTable( "c.directMenuItemPlacements", null, SelectBuilder.LEFT_JOIN_FETCH, null );
+            }
+            if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.CONTENT_HOME ) )
+            {
+                hqlQuery.addFromTable( "c.contentHomes", null, SelectBuilder.LEFT_JOIN_FETCH, null );
+            }
         }
-        if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.MAIN_VERSION ) )
-        {
-            hqlQuery.addFromTable( "c.mainVersion", null, SelectBuilder.LEFT_JOIN_FETCH, null );
-        }
-        if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.SECTION_CONTENT ) )
-        {
-            hqlQuery.addFromTable( "c.sectionContents", null, SelectBuilder.LEFT_JOIN_FETCH, null );
-        }
-        if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.DIRECT_MENUITEM_PLACEMENT ) )
-        {
-            hqlQuery.addFromTable( "c.directMenuItemPlacements", null, SelectBuilder.LEFT_JOIN_FETCH, null );
-        }
-        if ( contentEagerFetches.hasTable( ContentEagerFetches.Table.CONTENT_HOME ) )
-        {
-            hqlQuery.addFromTable( "c.contentHomes", null, SelectBuilder.LEFT_JOIN_FETCH, null );
-        }
+
         hqlQuery.addFilter( "AND", new InClauseBuilder<ContentKey>( "c.key", contentKeys )
         {
             public void appendValue( StringBuffer sql, ContentKey value )
@@ -73,4 +84,15 @@ class FindContentByKeysQuerier
         //noinspection unchecked
         return compiled.list();
     }
+
+    private boolean eagerFetchingIsSafe( final Collection<ContentKey> contentKeys )
+    {
+        final Number number = (Number) hibernateSession.createCriteria( SectionContentEntity.class ).
+            add( Restrictions.in( "content.key", contentKeys ) ).
+            setProjection( Projections.rowCount() ).
+            uniqueResult();
+
+        return number.intValue() <= ( EAGER_FETCH_NUMBER_OF_SECTIONS_THRESHOLD * contentKeys.size() );
+    }
+
 }
